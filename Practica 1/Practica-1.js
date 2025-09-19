4e3e8fce4fba19eca70c85ba90b07aa88591f255
kevin-hernandez-cmd <124051923@upq.edu.mx>
2025-09-18 20:17:19 -0600

    ejercicio a)
    es un saludo en donde identificamos cuando tenemos que ocupar let y const

---
+++ Practica 1/Practica-1.js
@@ -0,0 +1,11 @@
+
+// ejercicio a)
+let nombre = "Armando"; // puede cambiar, entonces usamos let
+const edad = 25;        // no cambia, entonces const
+
+nombre = "Ana Maria";   // en este caso es una reasignación 
+
+const saludo = `Hola, ${nombre}. Tienes ${edad} años.`; 
+
+console.log(saludo);
+
