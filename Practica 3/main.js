e273998db69efe02b2c717a80d56aff9114752d3
kevin-hernandez-cmd <124051923@upq.edu.mx>
2025-09-25 19:50:15 -0600

    ejercicio a) funcion
    se crea la funcion y la exportamos

---
+++ Practica 3/main.js
@@ -0,0 +1,8 @@
+/* se utiliza import para importar una funcion de otro archivo el cual es el FROM
+ despues se coloca el nombre del archivio*/
+
+import { restar } from './utils.js';
+
+console.log(restar(10, 5));  
+console.log(restar(20, 7));  
+console.log(restar(3, 9));   
---
+++ Practica 3/utils.js
@@ -0,0 +1,4 @@
+// se utiliza la funcion export para exportar la funcion a otro archivo
+export function restar(a, b) {
+  return a - b;
+}
