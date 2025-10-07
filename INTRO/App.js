513146d6776f6dd9e1afed77e83b70981330e45f
kevin-hernandez-cmd <124051923@upq.edu.mx>
2025-10-07 17:19:16 -0600

    Practica 5 Componentes Props y State

--- INTRO/App.js
+++ INTRO/App.js
@@ -1,15 +1,29 @@
+//1. Imports: Zona de importaciones
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button } from 'react-native-web';
 
+//2. Main: Zona de componentes
 export default function App() {
+
+  const[contador, setContador] = useState(0);
+  
+
   return (
+
+
     <View style={styles.container}>
-      <Text>Hola mundo react native!</Text>
+      <Text> Contador: {contador} </Text>
+      <Button title='Agregar' onPress={()=>setContador(contador+1)} />
+      <Button title='Quitar' onPress={()=>setContador(contador-1)} />
+      <Button title='Reiniciar' onPress={()=>setContador(0)} />
       <StatusBar style="auto" />
-    </View>
+    </View> // se tiliza para visualizar el texto
   );
 }
 
+//3. Estilos: Zona estilos
 const styles = StyleSheet.create({
   container: {
     flex: 1,
